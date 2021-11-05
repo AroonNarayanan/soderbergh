f95e507899c96015b0ffafbe86b9c44d508ddd83
Aroon Narayanan <aroon@gatech.edu>
2021-11-05 17:09:25 -0400

    Added television compatibility and tmdb fields

--- src/models.ts
+++ src/models.ts
@@ -4,14 +4,20 @@ export interface IEntry extends Document {
     title: string,
     year: number,
     type: string,
-    dateWatched: Date
+    episodeCount: number,
+    dateWatched: Date,
+    tmdbid: number,
+    posterUrl: string
 }
 
 const EntrySchema: Schema = new Schema({
     title: String,
     year: Number,
     type: String,
-    dateWatched: Date
+    episodeCount: Number,
+    dateWatched: Date,
+    tmdbid: Number,
+    posterUrl: String,
 }, {timestamps: true});
 
 const Entry = mongoose.model<IEntry>('Entry', EntrySchema)
